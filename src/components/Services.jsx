c57073241c94e57a0f423de1719488a7b050bfa1
adarsh3149 <adarshsohamacharya888@gmail.com>
2024-02-01 00:50:56 +0530

    first commit

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import AnimatedTitle from './AnimTitle'
-import { Box } from '@mui/material'
-import  CardComponent  from './CardComponent'
+import React from "react";
+import AnimatedTitle from "./AnimTitle";
+import { Box } from "@mui/material";
+import CardComponent from "./CardComponent";
 function Services() {
   return (
     <>
-    <div className="heading" style={{margin:"2rem"}}>
-          <AnimatedTitle name="Our Services ."></AnimatedTitle>
-        </div>
-    <Box sx={{
-        margin:"2rem",
-        padding: "0 1rem",
-        display: "flex",
-        flexWrap: "wrap",
-        rowGap:"1.2rem",
-        justifyContent: "space-evenly",
-        alignItems: "flex-start",
-    }}>
-        
-        <CardComponent/>
-    </Box>
+      <div className="heading" style={{ margin: "2rem" }}>
+        <AnimatedTitle name="Our Services ."></AnimatedTitle>
+      </div>
+      <Box
+        sx={{
+          margin: "2rem",
+          padding: "0 1rem",
+          display: "flex",
+          flexWrap: "wrap",
+          rowGap: "1.2rem",
+          justifyContent: "space-evenly",
+          alignItems: "flex-start",
+        }}
+      >
+        <CardComponent />
+      </Box>
     </>
-  )
+  );
 }
 
-export default Services
+export default Services;
