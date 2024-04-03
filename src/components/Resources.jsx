2eba5627ee27d70972ec25a46f8e194a1245a67a
adarsh3149 <adarshsohamacharya888@gmail.com>
2024-04-03 21:18:52 +0530

    resources loading fixed

--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect} from "react";
+import React,{useEffect, useState} from "react";
 import AnimatedTitle from "./AnimTitle";
 import { Box, Paper } from "@mui/material";
 import { CardContent, Card, Icon, Button } from "semantic-ui-react";
@@ -18,9 +18,18 @@ import {
 
 function Resources() {
   const dataArray = CardData_4;
-  const [open, setOpen] = React.useState(false);
-  const [curr, setCurrent] = React.useState();
-  const iframeRef = React.useRef(null);
+  const [open, setOpen] = useState(false);
+  const [curr, setCurrent] = useState(null);
+
+  const handleOpen = (item) => {
+    setCurrent(item);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  
   useEffect(() => {
     // Scroll to the top when ComponentB mounts
     window.scrollTo({
@@ -117,84 +126,61 @@ function Resources() {
           padding: "2rem",
           display: "flex",
           flexWrap: "wrap",
-          rowGap: "1.2rem",
-          columnGap: "1rem",
           justifyContent: "space-evenly",
-          alignItems: "flex-start",
         }}
       >
         {CardData_5.map((item, index) => (
-          <Modal
+          <motion.div
             key={index}
-            onClose={() => setOpen(false)}
-            onOpen={() => setOpen(true)}
-            open={open}
-            trigger={
-              <motion.div
-                key={index}
-                initial={{ opacity: 0, y: 50 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                viewport={{ once: true }}
-                transition={{
-                  duration: 0.1,
-                  type: "spring",
-                  stiffness: 80,
-                  delay: (index / 2) * 0.2 + 0.5,
-                }}
-              >
-                
-                  
-                    <Paper
-                      elevation={0}
-                      sx={{
-                        backgroundColor: "transparent",
-                        border: "0.1rem solid black",
-                        width: { xs: "24rem", md: "25rem" },
-                        padding: "2rem",
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "center",
-                        alignItems: "c",
-                        cursor:"pointer"
-                      }}
-                      key={item.id} onClick={() => setCurrent(item)}
-                    >
-                      <Box sx={{ fontWeight: "600", fontSize: "1.6rem" }}>
-                        {item.header}
-                      </Box>
-                    </Paper>
-               
-                
-              </motion.div>
-            }
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{
+              duration: 0.1,
+              type: "spring",
+              stiffness: 80,
+              delay: (index / 2) * 0.2 + 0.5,
+            }}
           >
-            <ModalHeader>
-              {curr && curr.header}
-              <ModalActions
-                style={{ position: "absolute", top: 10, right: 10 }}
-              >
-                <Icon link name="close" onClick={() => setOpen(false)} />
-              </ModalActions>
-            </ModalHeader>
-            <ModalContent image scrolling>
-              <ModalDescription>
-                <iframe
-                  title={item.header}
-                  src={curr && curr.url}
-                  width="100%"
-                  height="600px"
-                  frameBorder="0"
-                  marginHeight="0"
-                  marginWidth="0"
-                  ref={iframeRef}
-                >
-                  Loading...
-                </iframe>
-              </ModalDescription>
-            </ModalContent>
-          </Modal>
+            <Paper
+              elevation={0}
+              sx={{
+                backgroundColor: "transparent",
+                border: "0.1rem solid black",
+                width: { xs: "24rem", md: "25rem" },
+                padding: "2rem",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                cursor: "pointer",
+                margin: "1rem",
+              }}
+              key={index}
+              onClick={() => handleOpen(item)}
+            >
+              <div style={{ fontWeight: "600", fontSize: "1.6rem" }}>
+                {item.header}
+              </div>
+            </Paper>
+          </motion.div>
         ))}
       </div>
+
+      <Modal open={open} onClose={handleClose}>
+        <Modal.Header>{curr && curr.header}</Modal.Header>
+        <Modal.Content scrolling>
+          <Modal.Description>
+            <iframe
+              title={curr && curr.header}
+              src={curr && curr.url}
+              width="100%"
+              height="600px"
+              style={{ border: "none" }}
+            />
+          </Modal.Description>
+        </Modal.Content>
+      </Modal>
     </>
   );
 }
